4ee9ec47574bea05415821f72bad5048ced15632
vjsubash <vjsubash79@gmail.com>
2023-06-29 22:43:16 -0700

    sb-admin-crud

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from "./Card";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-import { Link,useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard({users,setUsers}) {
   let navigate = useNavigate()
